f386132c69b2aca667de25d2a72d2ec38ee41c84
Lucky Alvin Y. Casipe <luckyalvinyc@gmail.com>
2021-12-05 22:53:34 +0800

    moved backend related files to `backend` directory

--- backend/src/pg.js
+++ backend/src/pg.js
@@ -2,21 +2,17 @@ import postgres from 'postgres'
 
 import config from '../config.js'
 
-export default connect(config.postgres)
+export default postgres({
+  port: config.postgres.port,
+  username: config.postgres.username,
+  password: config.postgres.password,
+  database: config.postgres.database,
 
-export function connect (conf = config.postgres) {
-  return postgres({
-    port: conf.port,
-    username: conf.username,
-    password: conf.password,
-    database: conf.database,
-
-    onnotice (message) {
-      if (config.isTest) {
-        return
-      }
-
-      console.log(message)
+  onnotice (message) {
+    if (config.isTest) {
+      return
     }
-  })
-}
+
+    console.log(message)
+  }
+})
