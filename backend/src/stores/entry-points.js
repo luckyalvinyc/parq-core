d8578180c57b1ce9fc8893708eed762d8718d42f
Lucky Alvin Y. Casipe <luckyalvinyc@gmail.com>
2021-12-05 22:50:06 +0800

    backend: [WIP] tests for entryPoints

--- backend/src/stores/entry-points.js
+++ backend/src/stores/entry-points.js
@@ -1,12 +1,40 @@
-import { execute } from './utils.js'
+import sql from '../pg.js'
 
-export const tableName = 'entry_points'
+export const TABLE_NAME = 'entry_points'
+
+/**
+ * Creates entry points from the given labels
+ *
+ * @param {string[]} labels
+ * @returns {Promise<object[]>}
+ */
 
 export async function bulkCreate (labels) {
-  const sql = execute()
-
   return sql`
     INSERT INTO
-      ${sql(tableName)} ${sql(labels, 'label')}
+      ${sql(TABLE_NAME)} ${sql(labels, 'label')}
+    RETURNING
+      id,
+      label
   `
+}
+
+/**
+ * Checks if the given entry point exists
+ *
+ * @param {number} entryPointId
+ * @returns {Promise<boolean>}
+ */
+
+export async function exists (entryPointId) {
+  const [ entryPoint ] = await sql`
+    SELECT
+      id
+    FROM
+      ${sql(TABLE_NAME)}
+    WHERE
+      id = ${entryPointId}
+  `
+
+  return entryPoint !== undefined
 }
--- backend/src/stores/index.js
+++ backend/src/stores/index.js
@@ -1,2 +1,4 @@
 export * as slots from './slots.js'
 export * as tickets from './tickets.js'
+export * as entryPoints from './entry-points.js'
+
